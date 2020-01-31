f8fd4d12cee3357ad873be03e926371f7045dafa
bm51s <bm51s@ukr.net>
2020-01-31 18:18:06 +0200

    test3

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
-    <Card />
+    <Card v-for="item in company" v-bind:key="item.id" v-bind:info="item"/>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 import Card from "./components/Card";
 
 export default {
@@ -16,37 +16,35 @@ export default {
   data() {
     return {
       company: []
-    }
+    };
   },
-   mounted() {
-    this.getCompanyData()
+  mounted() {
+    this.getCompanyData();
   },
   methods: {
-
     async getCompanyData() {
-      let company = 'tsla',
-      token = "uojwh6sISmZX6qqbgzuh3jruxQJzKJJP9VxMhunjDIZEqJO770i8vAWtitKw";
-    axios
-      .get(
-        "https://api.worldtradingdata.com/api/v1/stock?symbol=" +
-          company +
-          "&api_token=" +
-          token
-      )
-      .then(res => {
-        // handle success
-        this.company = res.data.data;
-        console.log(this.company);
-      })
-      .catch(error => {
-        // handle error
-        console.log(error);
-      })
-      .finally(() => {
-        // always executed
-      });
+      let company_param = "snap,tsla",
+        token = "uojwh6sISmZX6qqbgzuh3jruxQJzKJJP9VxMhunjDIZEqJO770i8vAWtitKw";
+      axios
+        .get(
+          "https://api.worldtradingdata.com/api/v1/stock?symbol=" +
+            company_param +
+            "&api_token=" +
+            token
+        )
+        .then(res => {
+          // handle success
+          this.company = res.data.data;
+          console.log(this.company);
+        })
+        .catch(error => {
+          // handle error
+          console.log(error);
+        })
+        .finally(() => {
+          // always executed
+        });
     }
-    
   }
 };
 </script>
@@ -60,7 +58,7 @@ body {
   height: 100%;
 }
 .wrapper,
-#root {
+#app {
   margin: 100px;
   font-size: 0;
 }
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,36 +1,37 @@
 <template>
-      <div className="card">
-        <div className="card-name">{this.props.info.name}</div>
-        <div className="card-exchange">
-          <div className="card-exchange__short">
-            {this.props.info.stock_exchange_short}
+      <div class="card">
+        <div class="card-name">{{info.name}}</div>
+        <div class="card-exchange">
+          <div class="card-exchange__short">
+           {{info.stock_exchange_short}}
           </div>
-          <div className="card-exchange__symbol">{this.props.info.symbol}</div>
+          <div class="card-exchange__symbol">{{info.symbol}}</div>
         </div>
-        <div className="card-details">
-          <div className="card-details__price">{this.props.info.price}</div>
-          <div className="card-details__currency">
-            {this.props.info.currency}
+        <div class="card-details">
+          <div class="card-details__price">{{info.price}}</div>
+          <div class="card-details__currency">
+            {{info.currency}}
           </div>
-          <div className="card-details__change">
-            {this.props.info.day_change}
+          <div class="card-details__change">
+            {{info.day_change}}
           </div>
-          <div className="card-details__percent">
-            ({this.props.info.change_pct}%)
+          <div class="card-details__percent">
+            {{info.change_pct}}
           </div>
         </div>
         <div class="card-date">
           <div class="card-date__last-trading">
-            {this.props.info.last_trade_time}
+            {{info.last_trade_time}}
           </div>
-          <div class="card-date__timezone">{this.props.info.timezone}</div>
+          <div class="card-date__timezone">{{info.timezone}}</div>
         </div>
       </div>
 </template>
 
 <script>
 export default {
-  name: "Card"
+  name: "Card",
+  props: ['info']
 };
 </script>
 
